b69ea323d56c5417e9dd5ff8a82152418ef5490a
Artem-sudo25 <akritski@ymail.com>
2026-02-22 16:39:12 +0100

    posthog_proxy

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -2,5 +2,6 @@ import posthog from "posthog-js";
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
 	api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+	ui_host: process.env.NEXT_PUBLIC_POSTHOG_UI_HOST,
 	defaults: "2026-01-30",
 });
